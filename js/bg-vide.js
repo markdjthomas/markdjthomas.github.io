1a408bee94caab82d20aa68d24db9c15bb8220e8
Mark Thomas <markdjthomas@gmail.com>
2018-02-07 17:40:37 -0400

    dra web initial commit

---
+++ js/bg-vide.js
@@ -0,0 +1,13 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/bg.mp4",
+    poster: "img/bg-mobile-fallback.jpg"
+  }, {
+    posterType: 'jpg',
+    position: '50% 50%',
+  });
+
+})(jQuery); // End of use strict
