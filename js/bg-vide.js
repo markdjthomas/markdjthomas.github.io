d93a16767074123c9e029e1bbd16d1e924b4931c
Mark Thomas <markdjthomas@gmail.com>
2018-02-07 17:48:02 -0400

    bug fix for z-indices

--- js/bg-vide.js
+++ js/bg-vide.js
@@ -5,9 +5,5 @@
   $('body').vide({
     mp4: "mp4/bg.mp4",
     // poster: "img/bg-mobile-fallback.jpg"
-  }, {
-    posterType: 'jpg',
-    position: '50% 50%'
   });
-
 })(jQuery); // End of use strict
